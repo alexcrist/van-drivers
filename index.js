eaa43f77cf8b05499abd8d8bc92cbe92652fd633
alexcrist <alexecrist@gmail.com>
2016-12-17 18:39:16 -0800

    Test file routing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.json({'alex':'crist'});
+  res.send('index.html');
 });
 
 app.listen(app.get('port'), function() {
