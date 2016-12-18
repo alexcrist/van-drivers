ff2e07de46921f3274f2e992949a2807c7dcb929
alexcrist <alexecrist@gmail.com>
2016-12-17 18:23:03 -0800

    Trim app down to bare bones

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  res.json({alex:crist});
 });
 
 app.listen(app.get('port'), function() {
