c650582175a35218ea34bb69c72fedf6bdcf5237
alexcrist <alexecrist@gmail.com>
2018-01-14 18:28:03 -0800

    Keep free Heroku dyno awake via repeated pings

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express'),
+    http = require('http'),
     mongoose = require('mongoose'),
     Promise = require('bluebird'),
     slack = require('@slack/client'),
@@ -22,3 +23,9 @@ var mongoUri = process.env.MONGODB_URI || '';
 console.log('Connecting to MongoDB at ' + mongoUri);
 mongoose.Promise = Promise;
 mongoose.connect(mongoUri);
+
+// Ping site every 5 minutes to keep Heroku free dyno awake
+var FIVE_MINUTES = 300000;
+setInterval(function() {
+  http.get('http://van-drivers.herokuapp.com');
+}, FIVE_MINUTES);
